4b87ff1b9bf90232a63a20c80def8748c7e20af2
Gergely Koloszar <gergely.koloszar@gmail.com>
2025-08-01 18:14:10 +0200

    Add working main funcition

--- src/main.c
+++ src/main.c
@@ -8,60 +8,59 @@
 #include "configuration.h"
 #include "connector.h"
 #include "job-queue.h"
+#include "runtime.h"
+#include "worker-thread-functions.h"
+#include "workerpool.h"
 
 int main(int argc, const char* argv[]) {
 
-    // do we still run?
-    bool run = true;
-    
     // Generate default configuration
     Configuration main_config = configuration_generate_default_config();
 
     // parse configuration file and override defaults
     // if file is non existent, just go with the defaults
     if (config_file_exists(main_config.configuration.configuration_file_path)) {
-        int conf_ret =
-            parse_configuration_file(
-                                     &main_config,
-                                     main_config.configuration.configuration_file_path
-                                     );
-        if (conf_ret != 0) {
-            return -1;
-        }
+        int conf_ret = parse_configuration_file(
+            &main_config, main_config.configuration.configuration_file_path);
+        if (conf_ret != 0) { return -1; }
     }
 
     // parse command line arguments as final, top priority settings
-    int cmd_ret  = parse_cmdline_arguments(argc, argv, &main_config);
-    if (cmd_ret != 0) {
-        return -1;
-    }
+    int cmd_ret = parse_cmdline_arguments(argc, argv, &main_config);
+    if (cmd_ret != 0) { return -1; }
 
-    printf("Default config path: %s\n", main_config.configuration.configuration_file_path);
+    printf("Default config path: %s\n",
+           main_config.configuration.configuration_file_path);
     printf("Default executable name: %s\n", main_config.general.binary_name);
 
     JobQueue job_queue;
-    int jq_ret = job_queue_init(&job_queue, &main_config);
-    if (jq_ret != 0) {
-        return -1;
-    }
+    int      jq_ret = job_queue_init(&job_queue, &main_config);
+    if (jq_ret != 0) { return -1; }
 
     Connector connector;
-    if (connector_init(&connector, &main_config) != 0) {
-        return -1;
-    }
-    
-    if (connector_start(&connector) != 0) {
+    if (connector_init(&connector, &main_config) != 0) { return -1; }
+
+    Workerpool workerpool;
+    if (workerpool_init(&workerpool, &job_queue, &main_config,
+                        worker_thread_function) != 0) {
         return -1;
     }
 
-    while (run) {
-        if (connector_iterate(&connector, &job_queue) != 0) {
-            break;
-        }
+    if(workerpool_start(&workerpool) != 0) {
+        return -1;
+    }
+
+    if (connector_start(&connector) != 0) { return -1; }
+
+    setup_sigint();
+    program_start();
+    while (program_get_run_state()) {
+        if (connector_iterate(&connector, &job_queue) != 0) { break; }
     }
 
     connector_destroy(&connector);
-    
+    workerpool_destroy(&workerpool);
     configuration_destroy(&main_config);
+
     return 0;
 }
