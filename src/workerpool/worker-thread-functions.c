31bce69752e9a7f9f18d5344b4ad75b7f799e78e
Gergely Koloszar <gergely.koloszar@gmail.com>
2026-02-01 11:35:02 +0100

    feat: add hardcoded response to http requests

--- src/workerpool/worker-thread-functions.c
+++ src/workerpool/worker-thread-functions.c
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <arpa/inet.h>
+#include <string.h>
 
 void* worker_thread_function(void* arg) {
 
     JobQueue* job_queue = (JobQueue*)arg;
-    /* Job       job; */
 
     while(1) { // how to stop threads?
 
@@ -17,13 +18,40 @@ void* worker_thread_function(void* arg) {
         pthread_cond_wait(&job_queue->new_job_cond_v, &job_queue->new_job_cond_mux);
         pthread_mutex_unlock(&job_queue->new_job_cond_mux);
 
-        /* int ret = job_queue_pop(job_queue, &job); */
-        /* if (ret < 0) { */
-        /*     pthread_exit(NULL); */
-        /* } */
+        Job job;
+        int ret = job_queue_pop(job_queue, &job);
+        if (ret < 0) {
+            perror("job_queue_pop");
+            pthread_exit(NULL);
+        }
 
+        const char* response = "\
+HTTP/1.0 200 OK\r\n\
+Content-Type: text/html\r\n\
+Content-Length: 106\r\n\
+\r\n\r\n\
+<!DOCTYPE html>\r\n\
+<html>\r\n\
+<body>\r\n\
+<h1>My First Heading</h1>\r\n\
+<p>My first paragraph.</p>\r\n\
+</body>\r\n\
+</html>\r\n\
+            ";
+        size_t response_len = strlen(response);
+
+        ret = write(job.connection.socket, response, response_len);
+        if (ret < 0) {
+            perror("write");
+            pthread_exit(NULL);
+        } else {
+            char address_string[20];
+            memset(address_string, '\0', 20);
+            inet_ntop(AF_INET, &job.connection.address, address_string, 20);
+            printf("response sent to: %s\n", address_string);
+        }
+        
         /* // process http request */
-        printf("Thread %lu is running!\n", pthread_self());
         sleep(rand() % 3);
 
     }
